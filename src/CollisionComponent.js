faa9c97bc5d0b0ac4c8982ae8483d7a761095929
DJ O'Leary <djoleary39@ymail.com>
2018-10-23 11:07:27 +0100

    Added skeleton for component

---
+++ src/CollisionComponent.js
@@ -0,0 +1,6 @@
+/*! CollisionComponent v0.0.0 - MIT license */
+'use strict';
+
+var CollisionComponent = function () {
+  // your code goes here
+}
---
+++ test/CollisionComponent.js
@@ -0,0 +1,20 @@
+/* global CollisionComponent, describe, it, expect, should */
+
+describe('CollisionComponent()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(CollisionComponent).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
