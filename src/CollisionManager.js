14e000d20941549a14ab6f837cbb9bcb715e6b0c
DJ O'Leary <djoleary39@ymail.com>
2018-11-27 14:41:32 +0000

    Removed index

--- src/CollisionManager.js
+++ src/CollisionManager.js
@@ -353,17 +353,14 @@ class CollisionManager
    * @return {Boolean}
    */
   static CollidedWithTag(colliderIndex, collisionResults, checkArray, tag) {
-    var result = false;
-    if (index > -1) {
-      var collidedObjectTags = [];
-      for(var i = 0; i < collisionResults[colliderIndex].length; i++) {
-        if (collisionResults[colliderIndex][i] === true) {
-          collidedObjectTags.push.apply(collidedObjectTags, checkArray[i].objectTags);
-        }
+    var collidedObjectTags = [];
+    for(var i = 0; i < collisionResults[colliderIndex].length; i++) {
+      if (collisionResults[colliderIndex][i] === true) {
+        collidedObjectTags.push.apply(collidedObjectTags, checkArray[i].objectTags);
       }
-      result = CollisionManager.ArrayContainsElement(collidedObjectTags, tag);
     }
-    return result;
+    
+    return CollisionManager.ArrayContainsElement(collidedObjectTags, tag);;
   }
 
   /**
